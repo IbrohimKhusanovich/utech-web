0abb60b9e6a6e2717843e4ec4222f79279b25c79
Ibrohim <iyuldashev021@gmail.com>
2021-10-25 00:38:12 +0500

    completed swiper in landing

--- logics/main-page1.js
+++ logics/main-page1.js
@@ -3,7 +3,9 @@ const hamburgerMenu=document.querySelector('.menu-panel')
 const closeMenuIcon=document.querySelector('.nav-close-menu')
 hamburger.addEventListener('click', ()=>{
 hamburgerMenu.classList.add('doorMenu')
+document.body.style.overflow='hidden'
 })
 closeMenuIcon.addEventListener('click', ()=>{
  hamburgerMenu.classList.remove('doorMenu')
+ document.body.style.overflow='auto'
 })
