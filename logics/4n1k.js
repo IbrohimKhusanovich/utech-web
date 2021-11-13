183d166fd15caef3045ea6e674f954f8783e0974
Ibrohim <iyuldashev021@gmail.com>
2021-11-13 18:28:18 +0500

    created some pages

---
+++ logics/4n1k.js
@@ -0,0 +1,95 @@
+const content4n1k = document.querySelector(".sec4n1k-cards-box");
+
+const data = [
+  {
+    img: "../assets/images/main/card-img1.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img2.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img3.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img1.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img2.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img3.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img1.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img2.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+  {
+    img: "../assets/images/main/card-img3.png",
+    title: "Lorem Ipsum",
+    time: "12:00",
+    adress: "Zoom",
+    description:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Amet minim mollit non deserunt ullamco",
+  },
+];
+
+for (let i = 0; i < data.length; i++) {
+  content4n1k.innerHTML += `
+  <div class="swiper-slide">
+ <div class="card-item">
+<img class="card-item-img" src="${data[i].img}" alt="card-img">
+<div class="card-item-texts">
+<h4 class="card-item-title">${data[i].title}</h4>
+ <p class="card-item-arrow"></p>
+ <p class="card-item-description" >${data[i].description}</p>
+ <div class="card-item-bottom">
+    <button class="card-item-btn">Start free trial</button>
+
+        </div>
+        </div>
+        </div>
+        </div>
+        `;
+}
